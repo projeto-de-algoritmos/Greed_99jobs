93fea2b085642c4fc1008af495ae2e1670436b1b
Icaro Pereira <icarooliv2@gmail.com>
2021-09-05 19:16:41 -0300

    adds algorithm and table rendering
    Co-authored-by: Augusto Vilarins <augusto.vilarins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { useState} from 'react';
+import { Jumbotron, Container, Form, Button } from 'react-bootstrap';
 import './App.css';
 import ItemForm from './ItemForm';
 import JobsTable from './JobsTable';
-import {Job} from './interval';
+import { Job, scheduleJobs } from './interval';
 
 const App = () => {
   const [jobs, setJobs] = useState([]);
+  const [scheduledJobs, setOrderedJobs] = useState([]);
+  const [maxJobs, setMaxJobs] = useState(10);
+  const [profit, setProfit] = useState(0);
+
   const updateJobs = (newJob) => {
     const job = new Job(newJob.name, newJob.deadline, newJob.profit);    
     setJobs([...jobs, job]);
   }
+
+  const orderBySchedule = () => {
+    const {scheduledJobs, profit} = scheduleJobs(jobs, maxJobs);
+    setOrderedJobs(scheduledJobs);
+    setProfit(profit);
+  };
+
   return (
     <Container className="p-3">
       <Jumbotron>
@@ -19,6 +31,16 @@ const App = () => {
       <h3> Jobs Listing </h3>
       <JobsTable jobs={jobs}/>
       <h3> Schedule </h3>
+      <Form>
+        <Form.Group controlId="maxJobs">
+          <Form.Label>Max Jobs</Form.Label>
+          <Form.Control type="number" placeholder="Max Jobs" value={maxJobs} onChange={(e) => setMaxJobs(e.target.value)}/>
+        </Form.Group>
+      </Form>
+      <Button style={{marginTop: '5px'}} className="btn btn-primary" onClick={orderBySchedule}>Schedule</Button>
+      <h4> Schedule Listing </h4>
+      <span> Total profit: {profit} </span>
+      <JobsTable jobs={scheduledJobs}/>
     </Container>
   );
 };
--- src/interval.js
+++ src/interval.js
@@ -7,6 +7,29 @@ class Job {
     }
 }
 
+const scheduleJobs = (jobs, T) => {
+    let profit = 0;
+    let slot = new Array(T).fill(-1);
+    jobs.sort((a, b) => b.profit - a.profit);
+    for (let job of jobs) {
+        for (let j = job.deadline - 1; j >= 0; j--) {
+            if (j < T && slot[j] == -1) {
+                slot[j] = job.id;
+                profit += job.profit;
+                break;
+            }
+        }
+    }
+    const jobsIds = slot.filter(val => val != -1).map(val => val);
+    const scheduledJobs = jobsIds.map(id => jobs.find(job => job.id == id));
+
+    return {
+        scheduledJobs,
+        profit
+    }
+}
+
 module.exports = {
     Job,
+    scheduleJobs
 }
