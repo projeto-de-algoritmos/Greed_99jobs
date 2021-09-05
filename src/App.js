859292896267a4c40c1637577738cf2bd02ca0bc
Icaro Pereira <icarooliv2@gmail.com>
2021-09-05 18:15:58 -0300

    adds missing import
    Co-authored-by: Augusto Vilarins <augusto.vilarins@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState} from 'react';
 import './App.css';
+import {Job} from './interval';
+
 const App = () => {
   const [jobs, setJobs] = useState([]);
   const updateJobs = (newJob) => {
